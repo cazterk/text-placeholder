e1d27295d7180cec3d80adeab000d6f2b0c42f9d
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2021-10-06 09:34:18 +0200

    Update form.js

--- src/components/form.js
+++ src/components/form.js
@@ -1,17 +1,18 @@
 import React, { useState, useRef } from "react";
 
 export default function Form() {
+  const [input, setInput] = React.useState('')
   const ref = useRef("");
 
   const [val, setVal] = useState("[Place Holder]");
 
   function onClick() {
-    ref.current.value = `${val}  hi!`;
+    setInput(`${input} ${val}`)
   }
 
   return (
     <div>
-      <input ref={ref} />
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
       <button onClick={onClick}>Click</button>
     </div>
   );
